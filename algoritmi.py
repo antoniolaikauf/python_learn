defc069ec3888a555e4af2f25f3c67a5b3fa570c
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-06-22 21:53:27 +0200

    parola presente

--- algoritmi.py
+++ algoritmi.py
@@ -1,4 +1,3 @@
-# parola palindroma 
 def anagramma(str1,str2):
     if len(str2)==len(str1):
         return all(x in str2 for x in str1)
@@ -6,7 +5,6 @@ def anagramma(str1,str2):
 
 print(anagramma('heart','earth'))
 print(anagramma('heart','earph'))
-
 
 # Definire una funzione che riceva come argomenti due stringhe s1 e s2 contenenti la prima una frase
 # e la seconda una sequenza di lettere qualsiasi; la funzione dovrà restituire il numero di occorrenze
@@ -22,7 +20,6 @@ def string(s1,s2):
     return count 
 print(string('questo è un esercizio difficilissimo','ci'))
 
-
 # Definire una funzione che riceva come argomenti due liste che si assumono avere la stessa lunghezza,
 # contenenti i voti (numeri interi compresi tra 0 e 30) conseguiti da un certo insieme di studenti nelle
 # due prove di un esame. Posizioni corrispondenti nelle due liste si riferiscono allo stesso studente.
@@ -37,3 +34,27 @@ def arr_list(list1,list2):
             return False
     return True
 print(arr_list([21, 28, 26],[23, 12, 28]))
+
+
+# Definire una funzione che riceva come argomento una lista composta da dizionari, ciascuno dei quali
+# contiene le coordinate di un punto in uno spazio a tre dimensioni, associate alle chiavi "x", "y" e
+# "z"; per esempio: [{"x":2, "y":-1, "z":3}, {"x":1, "y":9, "z":0}].
+# La funzione dovrà restituire un dizionario contenente le coordinate del baricentro di tali punti,
+# che dovrà essere calcolato assumendo identiche le loro masse. Si ricorda che il baricentro di un
+# insieme di n punti {(x1, y1, z1),(x2, y2, z2), . . . ,(xn, yn, zn)} aventi massa identica è il punto avente
+# coordinate:
+
+point= [{"x":2, "y":-1, "z":3}, {"x":1, "y":9, "z":0}]
+
+def balance(br):
+    xBalance=0
+    yBalance=0
+    zBalance=0
+    for x in br:
+        xBalance+=x['x']
+        yBalance+=x['y']
+        zBalance+=x['z']
+    Cbalance=[{"x":xBalance/len(br), "y":yBalance/len(br), "z":zBalance/len(br)}]
+    return Cbalance
+
+print(balance(point))
