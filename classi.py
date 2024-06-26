cd75f1701492d5ec6381fecabdc290a9f74af8ae
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-06-26 11:10:39 +0200

    clasi

--- classi.py
+++ classi.py
@@ -1,4 +1,4 @@
-from abc import ABC # utilizzato per abstract delle classi https://docs.python.org/3/library/abc.html 
+from abc import ABC, abstractmethod # utilizzato per abstract delle classi https://docs.python.org/3/library/abc.html 
 
 class A:
    def __init__(self, c):
@@ -11,11 +11,32 @@ class A:
        return c
 
 print("Instantiating A..")
-# a = A(1)
-# print(a.alpha())
+a = A(1)
+print(a.alpha())
 
 '''
 si nota che l'output sarà Print inside A. anche se non si è inizializzata la classe A 
 Perché le dichiarazioni all'interno del corpo di una classe vengono eseguite
 indipendentemente dalla creazione dell'istanza. 
 '''
+
+
+class abstract(ABC):
+    @abstractmethod
+    def method(self):
+        pass
+
+'''
+un metdo con @abstractmethod e una funzione/metodo come method non può essere chiamato a meno che non si 
+usa un istanza di un figlio che sovvrascrive quel metodo 
+'''
+
+class child(abstract):
+    def method(self):
+        return 'funziono con il figlio e se sono sovrascritto'
+
+child1=child()
+print(child1.method())
+
+es=abstract()
+print(es.method())
